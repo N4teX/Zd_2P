2f045ce38b9fe6832f52bba4953048513c5d13d3
Kedarini <natan.waldowski@wp.pl>
2024-10-16 12:44:34 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,15 +2,31 @@
 {
     static void Main(string[] args)
     {
+        Console.Write("for: ");
         for (int i = 0; i < 4; i++)
         {
             Console.Write($"{i}, ");
         }
+
+        Console.WriteLine();
+
+        Console.Write("while: ");
         int j = 0;
         while (j<10)
         {
             j++;
             Console.Write($"{j}, ");
         }
+
+        Console.WriteLine();
+
+        Console.WriteLine("foreach:");
+        Console.Write("Wpisz tekst: "); string text = Console.ReadLine();
+        int ilosc = 0;
+        foreach (char x in text)
+        {
+            ilosc++;
+        }
+        Console.WriteLine($"Ilosc znakow: {ilosc}");
     }
 }
